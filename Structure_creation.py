2f4c050fb42c6106e9bd557b0e5235510ae4eb1d
Joerg Schaarschmidt <joerg.schaarschmidt@kit.edu>
2024-03-21 14:38:42 +0100

    Use real structure file as output

--- Structure_creation.py
+++ Structure_creation.py
@@ -1,9 +1,10 @@
-import shutil
-
-
-def copy_wano_to_structure():
-    shutil.copyfile('rendered_wano.yml', 'structure.yml')
+import yaml
+from ase.build import bulk
+from ase.io import write
 
 
 if __name__ == '__main__':
-    copy_wano_to_structure()
+    with open('rendered_wano.yml') as fh:
+        params = yaml.safe_load(fh)
+    struct = bulk(**params)
+    write(bulk, "structure.xyz")
